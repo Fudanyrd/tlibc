f003718a0da3466fc63872df37b9e02072d3b972
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2024-11-20 15:59:28 +0800

    [IMPL] recursive mkdir

--- tlibc/mkdir.c
+++ tlibc/mkdir.c
@@ -1,4 +1,44 @@
 #include "sys.h"
+#include "fcntl.h"
+#include <stdbool.h>
+
+/** Make directory recursively. */
+int mkdir(const char *dir) {
+    static char buf[256];
+    int i = 0;
+    int ret = 0;
+    bool is_end = true;
+
+    while (dir[i] != 0) {
+        buf[i] = dir[i];
+        if (dir[i] == '/') {
+            buf[i] = 0;
+            int fd = sys_open(buf, O_RDONLY);
+            if (fd < 0) {
+                // create this directory
+                ret |= sys_mkdir(buf, 0777);
+            } else {
+                sys_close(fd);
+            }
+            buf[i] = '/';
+            is_end = true;
+        } else {
+            is_end = false;
+        }
+        i++;
+    }
+
+    // make dir at last level
+    if (!is_end) {
+        int fd = sys_open(buf, O_RDONLY);
+        if (fd < 0) {
+            ret |= sys_mkdir(buf, 0777);
+        } else {
+            sys_close(fd);
+        }
+    }
+    return ret == 0 ? 0 : 1;
+}
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -6,8 +46,10 @@ int main(int argc, char **argv) {
         sys_exit(1);
     }
 
+    int ret = 0;
+
     for (int i = 1; i < argc; i++) {
-        sys_mkdir(argv[i], 0777);
+        ret |= mkdir(argv[i]);
     }
-    sys_exit(0);
+    return ret;
 }
